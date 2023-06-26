9d7ce4a06ff99dc6ff522a45a4570475b16a2a74
aokealy <aokealy@gmail.com>
2023-06-26 09:55:19 +0000

    create models for account admin page and create custom user forms

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
 # Register your models here.
+
+CustomAccount = get_user_model()
+
+class CustomAccountAdmin(UserAdmin):
+    add_form = CustomUserCreationForm 
+    form = CustomUserChangeForm
+    model = CustomAccount
+    list_display = ['email', 'username',]
+
+admin.site.register(CustomAccount, CustomAccountAdmin)
+
+
