a70c4042015d25e9ffe12fee7dfbdb0c1da593b9
aokealy <aokealy@gmail.com>
2023-06-27 08:47:06 +0000

    create new html pages and create views for them

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,17 @@
 from django.views.generic import TemplateView
 
+# Core template page
 class CorePageView(TemplateView):
     template_name = 'blogapp/core.html'
+
+# Blog page
+# class BlogPageView(TemplateView):
+#     template_name = 'blogapp/blog-section.html'
+
+# Contact page
+class ContactPageView(TemplateView):
+    template_name = 'blogapp/contact.html'
+
+# About page
+class AboutPageView(TemplateView):
+    template_name = 'blogapp/about.html'
