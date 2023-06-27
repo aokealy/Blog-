849c73c85a36dbef7a77cd40117bdb0a0b2f9879
aokealy <aokealy@gmail.com>
2023-06-27 10:15:38 +0000

    create model for blog and register in admin

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import EmailContact
 
 # Register your models here.
+
+@admin.register(EmailContact)
+class EmailContactAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "topic",
+    )
+
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class EmailContact(models.Model):
+    email = models.EmailField(max_length=250)
+    topic = models.CharField(max_length=150)
+    message = models.TextField(max_length=700)
+
